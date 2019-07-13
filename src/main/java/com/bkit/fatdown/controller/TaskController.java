3ce6992befe2279b23bff8d41f4035381ae510e5
yujian95 <clj9509@163.com>
2019-07-13 15:57:43 +0800

    完成任务控制器方法设计

--- src/main/java/com/bkit/fatdown/controller/TaskController.java
+++ src/main/java/com/bkit/fatdown/controller/TaskController.java
@@ -1,7 +1,9 @@
 package com.bkit.fatdown.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bkit.fatdown.dto.CommonResultDTO;
+import com.bkit.fatdown.entity.TbTaskList;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @file: TaskController
@@ -16,5 +18,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/task")
 public class TaskController {
 
-//    public CommonResultDTO
+    @ApiOperation("获取今天任务")
+    @CrossOrigin
+    @RequestMapping(value = "/listTodayTask", method = RequestMethod.GET)
+    public CommonResultDTO listTodayTask(@RequestBody Integer uid) {
+        return CommonResultDTO.failed();
+    }
+
+    @ApiOperation("获取所有任务")
+    @CrossOrigin
+    @RequestMapping(value = "/listTask", method = RequestMethod.GET)
+    public CommonResultDTO listTask() {
+        return CommonResultDTO.failed();
+    }
+
+    @ApiOperation("获取对应的任务详情")
+    @CrossOrigin
+    @RequestMapping(value = "/getTask/{id}", method = RequestMethod.GET)
+    public CommonResultDTO getTaskById(@PathVariable Integer id) {
+        return CommonResultDTO.failed();
+    }
+
+    @ApiOperation("添加任务")
+    @CrossOrigin
+    @RequestMapping(value = "/addTask", method = RequestMethod.POST)
+    public CommonResultDTO addTask(@RequestBody TbTaskList taskList) {
+        return CommonResultDTO.failed();
+    }
+
+    @ApiOperation("更新任务")
+    @CrossOrigin
+    @RequestMapping(value = "/updateTask", method = RequestMethod.POST)
+    public CommonResultDTO updateTask(@RequestBody TbTaskList taskList) {
+        return CommonResultDTO.failed();
+    }
+
+    @ApiOperation("删除任务")
+    @CrossOrigin
+    @RequestMapping(value = "/deleteTask", method = RequestMethod.DELETE)
+    public CommonResultDTO deleteTask(@RequestBody Integer id) {
+        return CommonResultDTO.failed();
+    }
+
 }
