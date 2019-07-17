c18490bd376e87a7ce314d464733b600f6fddc5e
YuJian <clj9509@163.com>
2019-07-17 10:03:55 +0800

    添加weappUtil注释

--- src/main/java/com/bkit/fatdown/utils/WeappUtil.java
+++ src/main/java/com/bkit/fatdown/utils/WeappUtil.java
@@ -10,14 +10,29 @@ package com.bkit.fatdown.utils;
  */
 
 public class WeappUtil {
-
-    // TODO 测试appId待修改
-
+    /**
+     * 微信小程序登录,session请求路径
+     */
     private static final String REQUEST_URL = "https://api.weixin.qq.com/sns/jscode2session";
+    /**
+     * 小程序appid
+     */
     private static final String APP_ID = "wx4243a0b576e90fd9";
+    /**
+     * 小程序会话密钥
+     */
     private static final String APP_SECRET = "126bf51300a399ff588ced60caa0ee57";
+    /**
+     * 默认登录类型
+     */
     private static final String GRANT_TYPE = "authorization_code";
 
+    /**
+     * 获取openid和Session
+     *
+     * @param code
+     * @return
+     */
     public static String getSessionKeyOrOpenId(String code) {
         String params = "appid=" + APP_ID + "&secret=" + APP_SECRET + "&js_code=" + code
                 + "&grant_type=" + GRANT_TYPE;
@@ -25,3 +40,4 @@ public class WeappUtil {
         return HttpRequestUtil.sendGet(REQUEST_URL, params);
     }
 }
+
