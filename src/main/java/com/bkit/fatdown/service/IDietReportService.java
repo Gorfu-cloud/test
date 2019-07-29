854f3d4d89893277324ceb460a1a93c97a44fad1
yujian95 <clj9509@163.com>
2019-07-30 00:28:08 +0800

    完善饮食报告注释

--- src/main/java/com/bkit/fatdown/service/IDietReportService.java
+++ src/main/java/com/bkit/fatdown/service/IDietReportService.java
@@ -19,76 +19,77 @@ public interface IDietReportService {
     /**
      * 创建饮食报告
      *
-     * @param report
-     * @return
+     * @param report 饮食报告
+     * @return 返回结果
      */
     Boolean insert(TbDietReport report);
 
     /**
      * 更新饮食报告
      *
-     * @param report
-     * @return
+     * @param report 饮食报告
+     * @return 返回结果
      */
     Boolean update(TbDietReport report);
 
     /**
      * 查看报告是否存在
      *
-     * @param date
-     * @param uid
-     * @param type
-     * @return
+     * @param date 报告日期
+     * @param uid  用户id
+     * @param type 报告类型：0早餐,1午餐,2晚餐,3加餐,4每日,5每周,6每月
+     * @return 返回报告数目
      */
     Integer countReport(Date date, Integer uid, Integer type);
 
     /**
      * 获取报告
      *
-     * @param date
-     * @param uid
-     * @param type
-     * @return
+     * @param date 报告日期
+     * @param uid  用户编号
+     * @param type 报告类型：0早餐,1午餐,2晚餐,3加餐,4每日,5每周,6每月
+     * @return 返回：报告内容
      */
     TbDietReport getDietReport(Date date, Integer uid, Integer type);
 
     /**
      * 获取列表
      *
-     * @param date
-     * @param uid
-     * @param type
-     * @return
+     * @param date 报告日期
+     * @param uid  用户编号
+     * @param type 报告类型：0早餐,1午餐,2晚餐,3加餐,4每日,5每周,6每月
+     * @return 返回 饮食报告列表
      */
     List<TbDietReport> listDietReport(Date date, Integer uid, Integer type);
 
     /**
      * 生成饮食报告
      *
-     * @param date
-     * @param uid
-     * @param type
-     * @return
+     * @param date 报告日期
+     * @param uid  用户编号
+     * @param type 报告类型：0早餐,1午餐,2晚餐,3加餐,4每日,5每周,6每月
+     * @return 返回 饮食报告
      */
     UserReportDTO generateDietReport(Date date, Integer uid, Integer type);
 
     /**
      * 查找特定报告数
      *
-     * @param startDate
-     * @param endDate
-     * @param uid
-     * @param typeList
-     * @return
+     * @param startDate 开始日期 0：00
+     * @param endDate   结束日期 23：59
+     * @param uid       用户编号
+     * @param typeList  报告类型：0早餐,1午餐,2晚餐,3加餐,4每日,5每周,6每月
+     * @return 返回 报告数目
      */
     Integer countReportByTypeList(Date startDate, Date endDate, Integer uid, List<Integer> typeList);
+
 
     /**
      * 查找特定记录
      *
-     * @param date
-     * @param uid
-     * @return
+     * @param date 报告日期
+     * @param uid  用户编号
+     * @return 返回某天报告数
      */
     Integer countReportByDay(Date date, Integer uid);
 }
