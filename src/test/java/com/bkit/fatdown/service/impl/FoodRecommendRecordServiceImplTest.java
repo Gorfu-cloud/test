00414d6da0dfa4d597d9dbb6f420aab77aac67ea
YuJian <clj9509@163.com>
2019-10-10 14:34:52 +0800

    test(菜式推荐模块): 测试周菜式推荐功能

---
+++ src/test/java/com/bkit/fatdown/service/impl/FoodRecommendRecordServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.bkit.fatdown.service.impl;
+
+import cn.hutool.core.date.DateUtil;
+import com.bkit.fatdown.dto.food.RecommendListDTO;
+import com.bkit.fatdown.service.IFoodRecommendRecordService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class FoodRecommendRecordServiceImplTest {
+
+    @Resource
+    private IFoodRecommendRecordService service;
+
+    @Test
+    public void getWeeklyRecommend() {
+        Date date = DateUtil.parseDate("2019-08-15");
+        int uid = 65;
+        RecommendListDTO food =service.getWeeklyRecommend(date,uid);
+        assertEquals(4,food.getMore().size());
+        assertEquals(0,food.getLack().size());
+        assertNotNull(food);
+    }
+}
